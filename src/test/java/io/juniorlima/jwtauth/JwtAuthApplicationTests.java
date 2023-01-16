cb0892656caae1552598307a50eae5e092799c33
Junior Lima <josealbertorodriguesjunior@gmail.com>
2023-01-16 17:14:45 +0000

    Initial commit

---
+++ src/test/java/io/juniorlima/jwtauth/JwtAuthApplicationTests.java
@@ -0,0 +1,11 @@
+package io.juniorlima.jwtauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
